6ebb80fe7faabcdb9aeace9a440f00c9a8c6c569
Thomas Vinadelle <thomas.vinadelle@gmail.com>
2024-09-17 15:25:00 +0200

    Add source folder

---
+++ Source/SkillTest_M4/SkillTest_M4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SkillTest_M4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SkillTest_M4, "SkillTest_M4" );
